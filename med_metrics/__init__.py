172eb85a93dc74492e3c491ec3e146ccee641344
Erkin Otles <eotles@gmail.com>
2024-01-15 09:13:28 -0500

    Update __init__.py

--- med_metrics/__init__.py
+++ med_metrics/__init__.py
@@ -1,16 +1,25 @@
 """
-med_metrics Package
+Python tools for evaluating medical ML models
+==================================
 
 This package provides tools for evaluating machine learning models, 
 specifically designed for use in medical applications. It includes 
 functions for calculating various metrics, performing bootstrap evaluations, 
 and utility functions to support these tasks.
 
-Author: Erkin Ötleş
-Email: hi@eotles.com
 """
 
+Author: Erkin Ötleş
+Email: hi@eotles.com
+
 from .metrics import *
-from .curves import * 
+from .curves import *
 from .compatibility_metrics import *
 from .bootstrap import *
+
+__all__ = [
+    'metrics',
+    'curves',
+    'compatibility_metrics',
+    'bootstrap'
+]
